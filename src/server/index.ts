8c1f45bddefdac34e79ddfbe4740cd5bb995367c
ginnyTheCat <ginnythecat@lelux.net>
2021-03-13 22:16:45 +0100

    Updated index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,17 @@
 require("../const");
 
-import { createApi } from "./api";
-import { loadPlugins } from "./plugin";
 import { Logger } from "../logger";
+import { createApi } from "./api";
+import { Home, homes } from "./home";
 
 export async function startServer(port: string) {
   Logger._.info("Starting server");
 
   const app = createApi();
 
-  await loadPlugins();
+  // tmp
+  const home = new Home("123", "My Home");
+  homes.set(home.id, home);
 
   app.listen(port, () => {
     Logger._.info(`Listening on port ${port}`);
